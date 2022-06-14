331de13ac99a3dee56e4616caf2aec077570a40e
xopper <ali.haloua@usmba.ac.ma>
2022-06-14 16:12:14 +0100

    Add system prefernses as default theme mode - JSX fixes

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -41,27 +41,23 @@ function Navigation() {
       </div>
       <div className="Navigation-right w-full md:w-auto flex items-center gap-x-6">
         <label
-          for="default-toggle"
-          class="inline-flex relative items-center cursor-pointer"
+          htmlFor="default-toggle"
+          className="inline-flex relative items-center cursor-pointer"
         >
           <input
             type="checkbox"
             value=""
             id="default-toggle"
-            class="sr-only peer"
+            className="sr-only peer"
             checked={!darkLogo}
             onChange={() => {
-              if (darkLogo) {
-                Cookies.set("darkMode", "true");
-              } else {
-                Cookies.set("darkMode", "false");
-              }
+              Cookies.set("darkMode", darkLogo);
               document.querySelector("html").classList.toggle("dark");
               setDarkLogo(!darkLogo);
             }}
           />
-          <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-sky-600"></div>
-          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-sky-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
             Dark Mode
           </span>
         </label>
